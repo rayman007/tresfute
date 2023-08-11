3eff5cd37bc1f5fd5e2a8b117c13aab3325e6821
rayman007 <rayman007@rayman007>
2023-08-12 01:34:26 +0200

    Close modal on value selection

--- src/ClickableBoxNStates.js
+++ src/ClickableBoxNStates.js
@@ -1,25 +1,29 @@
 import './ClickableBoxes.css';
 
 import { useState } from 'react';
+import { useRef } from 'react';
 
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 export default function ClickableBoxNStates({info}) {
   
+  const ref = useRef(null);
   const [value, setValue] = useState(info.info.default_value);
 
   const handleClick = (e, value) => {
     e.preventDefault();
     if (info.checkLegal(info, value)) {
       setValue(value);
-      info.setValue(info, value)
+      info.setValue(info, value);
+      ref.current.close()
     }
   }
   const buttons = info.info.states.map((state) => <button class="numbers" onClick={(e) => {handleClick(e, state.v)}}>{state.t}</button>)
 
   return (
     <Popup
+      ref={ref}
       trigger={
         <div 
           key={info.name}
@@ -27,7 +31,6 @@ export default function ClickableBoxNStates({info}) {
           style={{top: info.py, left: info.px, width: info.w, height: info.h}}>{value === 0 ? "" : value}</div>
       }
       modal
-      nested
       closeOnDocumentClick
       >
         {close => (
