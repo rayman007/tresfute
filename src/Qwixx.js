b8b97f68fe25d0265ddf6228bce0c48390020a86
rayman007 <rayman007@rayman007>
2023-08-14 22:27:07 +0200

    Refacto qwixx with new widgets

--- src/Qwixx.js
+++ src/Qwixx.js
@@ -1,58 +1,11 @@
 import './Qwixx.css';
+import CaptionBox from './CaptionBox.js';
 import ClickableBox2States from './ClickableBox2States.js'
 import create_all_elements from './helpers.js';
 
 import { useState } from 'react';
 
 function Qwixx() {
-  
-  const all_info = [
-    
-    {    
-      comp: ClickableBox2States,
-      section: "red",
-      x: 55, w: 75, mx: 3.7,
-      y: 38, h: 75, my: 59,
-      nx: 12,  ny: 1,
-      avoid: []
-    },
-    
-    {
-      comp: ClickableBox2States,
-      section: "yellow",
-      x: 55, w: 75, mx: 3.7,
-      y: 142, h: 75, my: 59,
-      nx: 12,  ny: 1,
-      avoid: []
-    },
-    
-    {
-      comp: ClickableBox2States,
-      section: "green",
-      x: 55, w: 75, mx: 3.7,
-      y: 246, h: 75, my: 59,
-      nx: 12,  ny: 1,
-      avoid: []
-    },
-    
-    {
-      comp: ClickableBox2States,
-      section: "blue",
-      x: 55, w: 75, mx: 3.7,
-      y: 350, h: 75, my: 59,
-      nx: 12,  ny: 1,
-      avoid: []
-    },
-    
-    {
-      comp: ClickableBox2States,
-      section: "fail",
-      x: 839, w: 30, mx: 13,
-      y: 490, h: 39, my: 59,
-      nx: 4,  ny: 1,
-      avoid: []
-    }
-  ];
   
   const [values, setValues] = useState({
     red: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
@@ -118,22 +71,89 @@ function Qwixx() {
     }
   }
   
+  let scores = computeScore(values)
+
+  const all_info = [
+    
+    {    
+      comp: ClickableBox2States,
+      section: "red",
+      x: 55, w: 75, mx: 3.7,
+      y: 38, h: 75, my: 59,
+      nx: 12,  ny: 1,
+      avoid: []
+    },
+    
+    {
+      comp: ClickableBox2States,
+      section: "yellow",
+      x: 55, w: 75, mx: 3.7,
+      y: 142, h: 75, my: 59,
+      nx: 12,  ny: 1,
+      avoid: []
+    },
+    
+    {
+      comp: ClickableBox2States,
+      section: "green",
+      x: 55, w: 75, mx: 3.7,
+      y: 246, h: 75, my: 59,
+      nx: 12,  ny: 1,
+      avoid: []
+    },
+    
+    {
+      comp: ClickableBox2States,
+      section: "blue",
+      x: 55, w: 75, mx: 3.7,
+      y: 350, h: 75, my: 59,
+      nx: 12,  ny: 1,
+      avoid: []
+    },
+    
+    {
+      comp: ClickableBox2States,
+      section: "fail",
+      x: 839, w: 30, mx: 13,
+      y: 490, h: 39, my: 59,
+      nx: 4,  ny: 1,
+      avoid: []
+    },
+
+    {
+      comp: CaptionBox,
+      section: "scores",
+      x: 121, w: 70, mx: 51,
+      y: 550, h: 70, my: 80,
+      nx: 4,  ny: 1,
+      value: "0",
+      fsize: "40px",
+      values: [[scores.red, scores.yellow, scores.green, scores.blue]],
+      color: [["red", "gold", "green", "blue", "gray", "black"]],
+      avoid: []
+    },
+
+    {
+      comp: CaptionBox,
+      section: "scores2",
+      x: 631, w: 70, mx: 91,
+      y: 550, h: 70, my: 80,
+      nx: 2,  ny: 1,
+      value: "0",
+      fsize: "40px",
+      values: [[scores.fail, scores.total]],
+      color: [["gray", "black"]],
+      avoid: []
+    },
+];
   
   let boxes = create_all_elements(all_info, legalChecker, setValue);
-  
-  let scores = computeScore(values)
-  
+
   return (
     <div className="qwixx">
-    <div className="qwixx_board">
-    {boxes}
-    <div className='qwixx_score_box qwixx_red_score'>{scores.red}</div>
-    <div className='qwixx_score_box qwixx_yellow_score'>{scores.yellow}</div>
-    <div className='qwixx_score_box qwixx_green_score'>{scores.green}</div>
-    <div className='qwixx_score_box qwixx_blue_score'>{scores.blue}</div>
-    <div className='qwixx_score_box qwixx_gray_score'>{scores.fail}</div>
-    <div className='qwixx_score_box qwixx_black_score'>{scores.total}</div>
-    </div>
+      <div className="qwixx_board">
+        {boxes}
+      </div>
     </div>
     );
   }
